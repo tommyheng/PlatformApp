87d60730fd8a123fd3d20b3af2f92d100dcad8f3
tommyheng <tommyheng@DESKTOP-KMBO2BH>
2017-03-27 21:11:19 +0800

    添加项目文件。

---
+++ PlatformApp/Views/LoginView.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TomNet.Platform.App.Views
+{
+    /// <summary>
+    /// LoginView.xaml 的交互逻辑
+    /// </summary>
+    public partial class LoginView :Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void closeBtne_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void minBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void LoginView_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            this.LoginName.Focus();
+            LoginName.SelectionStart = LoginName.Text.Length;
+            double workHeight = SystemParameters.WorkArea.Height;
+            double workWidth = SystemParameters.WorkArea.Width;
+            this.Top = (workHeight - this.Height) / 2 - this.Height / 4;
+            this.Left = (workWidth - this.Width) / 2;
+        }
+        private void ForgetClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RegisterClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LoginClick(object sender, RoutedEventArgs e)
+        {
+            Hide();
+            MenuView menu = new MenuView();
+            menu.ShowDialog();
+            
+        }
+    }
+}
---
+++ PlatformApp/Views/MenuView.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TomNet.Platform.App.Views
+{
+    /// <summary>
+    /// MenuView.xaml 的交互逻辑
+    /// </summary>
+    public partial class MenuView 
+    {
+        public MenuView()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void MetroWindow_Closed(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MinBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void UIElement_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            double workHeight = SystemParameters.WorkArea.Height;
+            double workWidth = SystemParameters.WorkArea.Width;
+            this.Top = 80;
+            this.Left = (workWidth - this.Width)-100;
+        }
+    }
+}
