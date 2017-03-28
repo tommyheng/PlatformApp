619cd5afadf8c59dc79eba28262d7b84b94e7dc5
tanghang <zh@win-stock.com.cn>
2017-03-28 08:26:00 +0800

    update

--- PlatformApp/MainWindow.xaml.cs
+++ PlatformApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using CefSharp;
+using CefSharp.Wpf;
 using MahApps.Metro;
 using System;
 using System.Collections.Generic;
@@ -30,9 +31,10 @@ namespace PlatformApp
         private void WindowLoaded(object sender, RoutedEventArgs e)
         {
             ChangeTheme();
-            var setting = new CefSharp.CefSettings();
-            CefSharp.Cef.Initialize(setting, true, false);
+            var setting = new CefSettings();
+            Cef.Initialize(setting, true, false);
             var webView = new CefSharp.Wpf.ChromiumWebBrowser();
+            webView.LifeSpanHandler = new LifeSpanHandler();
             webView.FrameLoadEnd += WebViewLoaded;
             MainGrid.Children.Insert(0, webView);
             webView.Address = "http://www.zi-han.net/theme/hplus/";
@@ -51,6 +53,23 @@ namespace PlatformApp
                 maskLoading.Visibility = Visibility.Collapsed;
             }));
         }
+    }
 
+
+    internal class LifeSpanHandler : ILifeSpanHandler
+    {
+        public void OnBeforeClose(IWebBrowser browser)
+        {
+
+        }
+
+        public bool OnBeforePopup(IWebBrowser browser, string sourceUrl, string targetUrl, ref int x, ref int y, ref int width, ref int height)
+        {
+            //在这里，可以做你想做的事情哦
+
+            var chromiumWebBrowser = (ChromiumWebBrowser)browser;
+            chromiumWebBrowser.Load(targetUrl);
+            return true;
+        }
     }
 }
